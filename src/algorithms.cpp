471aa22ed33034c92b8774cc980989d1cbab3887
dandia42 <davidandiapardo@gmail.com>
2026-06-14 21:50:37 +0200

    Se actualizan comentarios en algorithms

--- src/algorithms.cpp
+++ src/algorithms.cpp
@@ -16,9 +16,8 @@ void Algorithm::dijkstra(int inicio, Graph* graph, bool sem) {
   } else
     cout << "\nEl inicio del graph es " << inicio << endl;
 
-  if (sem) {  // aplicar sobre el graph original
-    // Inicializar las distancias de los vertices
-    // inicio=2;
+  if (sem) {  // apply on original graph
+    // Initialize nodes distance
     for (int i = 0; i < graph->nNodes_; i++) {
       if (i == inicio)
         graph->node_[i].dijkstraVariable_.setDistance(0);
@@ -27,9 +26,9 @@ void Algorithm::dijkstra(int inicio, Graph* graph, bool sem) {
       graph->node_[i].dijkstraVariable_.setVisited(false);
     }
 
-    // Procesar los nodos en orden creciente de distance_
+    // Process nodes in increasing distance order
     for (int i = 0; i < graph->nNodes_; i++) {
-      // Encontrar el nodo más cercano no visited_
+      // Finde closer no visited node
       int u = -1;
       for (int j = 0; j < graph->nNodes_; j++)
         if (!graph->node_[j].dijkstraVariable_.getVisited() && 
@@ -39,7 +38,7 @@ void Algorithm::dijkstra(int inicio, Graph* graph, bool sem) {
       if (graph->node_[u].dijkstraVariable_.getDist() == MAX_WEIGHT) break;
       graph->node_[u].dijkstraVariable_.setVisited(true);
 
-      // Actualizar las distancias de los nodos adyacentes
+      // Update adjacent nodes distance
       for (int j = 0; j < graph->nLinks_; j++)
         if (graph->link_[j].getOrigin() == u) {
           int v = graph->link_[j].getDestination();
@@ -52,16 +51,15 @@ void Algorithm::dijkstra(int inicio, Graph* graph, bool sem) {
         }
     }
 
-    for (int i = 0; i < graph->nNodes_; i++)  // comprobar que se han visited_ todos los nodos
-      if (!graph->node_[i].dijkstraVariable_.getVisited()) cout << "\n\tFalta por visitar el node_ " << i;
-    DOUT << "\n\tSe han visited_ todos los nodos" << endl;
+    for (int i = 0; i < graph->nNodes_; i++)  // check that all nodes have been visited
+      if (!graph->node_[i].dijkstraVariable_.getVisited()) cout << "\n\tThe node: " << i << " has yet to be visited";
+    DOUT << "\n\tAll nodes visited" << endl;
     for (int i = 0; i < graph->nNodes_; i++)
-      DOUT << "\nVertice=" << i << "   Distancia=" << graph->node_[i].getDijkstraVariable_().getDist()
-           << "   predecessorID_=" << graph->node_[i].getDijkstraVariable_().getID() << "   iteration_=" 
+      DOUT << "\nNode=" << i << "   Distance=" << graph->node_[i].getDijkstraVariable_().getDist()
+           << "   predecessorID=" << graph->node_[i].getDijkstraVariable_().getID() << "   iteration=" 
            << graph->node_[i].getDijkstraVariable_().getIteration();
-  } else {  // aplicar sobre el graph conjugado
-    // Inicializar las distancias de los vertices
-    // inicio=2;
+  } else {  // apply on conjugate graph
+    // Initialize nodes distance
     for (int i = 0; i < graph->nConjugateNodes_; i++) {
       if (i == inicio)
         graph->conjugateNode_[i].dijkstraVariable_.setDistance(0);
@@ -70,9 +68,9 @@ void Algorithm::dijkstra(int inicio, Graph* graph, bool sem) {
       graph->conjugateNode_[i].dijkstraVariable_.setVisited(false);
     }
 
-    // Procesar los nodos en orden creciente de distance_
+    // Process nodes in increasing distance order
     for (int i = 0; i < graph->nConjugateNodes_; i++) {
-      // Encontrar el nodo más cercano no visited_
+      // Finde closer no visited node
       int u = -1;
       for (int j = 0; j < graph->nConjugateNodes_; j++)
         if (!graph->conjugateNode_[j].dijkstraVariable_.getVisited() &&
@@ -81,7 +79,7 @@ void Algorithm::dijkstra(int inicio, Graph* graph, bool sem) {
       if (graph->conjugateNode_[u].dijkstraVariable_.getDist() == MAX_WEIGHT) break;
       graph->conjugateNode_[u].dijkstraVariable_.setVisited(true);
 
-      // Actualizar las distancias de los nodos adyacentes
+      // Update adjacent nodes distance
       for (int j = 0; j < graph->nConjugateLinks_; j++)
         if (graph->conjugateLink_[j].getOrigin() == u) {
           int v = graph->conjugateLink_[j].getDestination();
@@ -94,38 +92,38 @@ void Algorithm::dijkstra(int inicio, Graph* graph, bool sem) {
         }
     }
 
-    for (int i = 0; i < graph->nConjugateNodes_; i++)  // comprobar que se han visited_ todos los nodos
-      if (!graph->conjugateNode_[i].dijkstraVariable_.getVisited()) cout << "\n\tFalta por visitar el node_ " << i;
-    cout << "\n\tSe han visited_ todos los nodos" << endl;
+    for (int i = 0; i < graph->nConjugateNodes_; i++)  // check that all nodes have been visited
+      if (!graph->conjugateNode_[i].dijkstraVariable_.getVisited()) cout << "\n\tThe node: " << i << " has yet to be visited";
+    DOUT << "\n\tAll nodes visited" << endl;
     for (int i = 0; i < graph->nConjugateNodes_; i++)
-      cout << "\nVertice=" << i << "   Distancia=" << graph->conjugateNode_[i].getDijkstraVariable_().getDist()
-           << "   predecessorID_=" << graph->conjugateNode_[i].getDijkstraVariable_().getID() << "   iteration_=" << graph->conjugateNode_[i].getDijkstraVariable_().getIteration();
+      cout << "\nNode=" << i << "   Distance=" << graph->conjugateNode_[i].getDijkstraVariable_().getDist()
+           << "   predecessorID=" << graph->conjugateNode_[i].getDijkstraVariable_().getID() << "   iteration=" << graph->conjugateNode_[i].getDijkstraVariable_().getIteration();
   }
 }
 
 void Algorithm::floydWarshall(Graph* graph) {
-  // matriz dist para las distancias entre todos los nodos, matriz origen para conocer el node_ previo
-  // Inicializar las distancias y los origenes de los vertices
+  // Weight matrix to save distance between nodes, origin matrix to save previous node
+  // Initialize distances and origins of the nodes
   for (int i = 0; i < graph->nConjugateNodes_; i++) {
     for (int j = 0; j < graph->nConjugateNodes_; j++) {
       graph->weightFloydWarshall_[i][j] = MAX_WEIGHT;
       graph->originFloydWarshall_[i][j] = -1;
     }
-    graph->weightFloydWarshall_[i][i] = 0;  // la distance_ a si mismo es 0
+    graph->weightFloydWarshall_[i][i] = 0;  // The distance to itself is 0
   }
 
-  // Actualizar las distancias a los valores iniciales del graph
+  // Update distance to initial graph values
   for (int i = 0; i < graph->nConjugateNodes_; i++) {
     for (int j = 0; j < graph->conjugateNode_[i].numberAdjacentNodes_; j++) {
-      // Se asignan las distancias con una matriz donde se ve el peso entre el node_ origen i y el node_ destino
+      // Distance between origin node i and destination node are assigned with a matrix
       // graph.conjugateNode_[i]->adjacentNode_[j]
       graph->weightFloydWarshall_[i][graph->conjugateNode_[i].adjacentNode_[j]] = graph->conjugateNode_[i].adjacentNodeWeight_[j];
       graph->originFloydWarshall_[i][graph->conjugateNode_[i].adjacentNode_[j]] =
-          i;  // se marca el origen y no se toca lo demas para la primera iteracion
+          i;  // check the origin and do not change anything on the first iteration
     }
   }
 
-  // Se aplica el algoritmo
+  // Apply algorithm
   for (int k = 0; k < graph->nConjugateNodes_; k++) {
     for (int i = 0; i < graph->nConjugateNodes_; i++) {
       for (int j = 0; j < graph->nConjugateNodes_; j++) {
@@ -138,23 +136,23 @@ void Algorithm::floydWarshall(Graph* graph) {
     }
   }
 
-  // Se obtiene el resultado
-  cout << "\nDistancias mínimas:" << endl;
+  // Obtain the result
+  cout << "\nMinimum distances:" << endl;
   for (int i = 0; i < graph->nConjugateNodes_; i++) {
     for (int j = 0; j < graph->nConjugateNodes_; j++) {
       if (graph->weightFloydWarshall_[i][j] == MAX_WEIGHT)
-        cout << "No conectan ";
+        cout << "Do not connect ";
       else
         cout << graph->weightFloydWarshall_[i][j] << ((graph->weightFloydWarshall_[i][j] > 9) ? ("  ") : ("   "));
     }
     cout << endl;
   }
 
-  cout << "\nMatriz de origenes:" << endl;
+  cout << "\nOrigin matrix:" << endl;
   for (int i = 0; i < graph->nConjugateNodes_; i++) {
     for (int j = 0; j < graph->nConjugateNodes_; j++) {
       if (graph->originFloydWarshall_[i][j] == MAX_WEIGHT)
-        cout << "No conectan ";
+        cout << "Do not connect ";
       else
         cout << graph->originFloydWarshall_[i][j] << ((graph->originFloydWarshall_[i][j] > 9) ? ("  ") : ("   "));
     }
@@ -162,71 +160,71 @@ void Algorithm::floydWarshall(Graph* graph) {
   }
 }
 
-void Algorithm::greenWave(Graph* graph, int*& rsem, int& ncompleta,
-                          StreetList& lca) {  // ciclo en el que aplicar el algoritmo
+void Algorithm::greenWave(Graph* graph, int*& streetlightRoute, int& streetlightNumber,
+                          StreetList& streetList) {  // loop to apply the algorithm
   int ini1 = rand() % graph->nNodes_, ini2 = rand() % graph->nNodes_,
-      ini3 = rand() % graph->nNodes_;         // se hace sobre el graph inicial, no el conjugado
-  bool aux1 = true;                         // flag para obtener un ini3 que no se repita
-  int n1 = 0, n2 = 0, n3 = 0;               // number de nodos en cada route_
-  int r_ = 0, *r1 = &r_, *r2 = &r_, *r3 = &r_;  // las 3 rutas
-  int *streets_, *calles1, *calles2, *calles3;
+      ini3 = rand() % graph->nNodes_;         // using the initial graph, not the congugated
+  bool aux1 = true;                         // flag to obtain ini3 that does not repeat
+  int n1 = 0, n2 = 0, n3 = 0;               // nodes number in each route
+  int route = 0, *r1 = &route, *r2 = &route, *r3 = &route;  // 3 routes
+  int *streets, *streets1, *streets2, *streets3;
 
-  while (ini1 == ini2)  // evitar que los indices desde los que se busca la mejor route_ sean los mismos
+  while (ini1 == ini2)  // avoid initial nodes repetition
     ini2 = rand() % graph->nNodes_;
 
-  DOUT << "\nesto no deberia coincidir ninguno" << endl;
-  auxFunction(ini1, ini2, graph, n1, r1, calles1);
+  DOUT << "\nThey should not repeat" << endl;
+  auxFunction(ini1, ini2, graph, n1, r1, streets1);
 
-  while (aux1) {  // tambien se podria aplicar usando std::find metiendole una lista de exclusion
+  while (aux1) {  // TODO tambien se podria aplicar usando std::find metiendole una lista de exclusion
     ini3 = rand() % graph->nNodes_;
     aux1 = 0;
     for (int i = 0; i < n1; i++)
       if (ini3 == r1[i]) aux1 = 1;
     if (ini3 == ini2) aux1 = 1;
   }
-  graph->resetGraph();  // se inicializan a 0 las variables de dijkstra
-  auxFunction(ini2, ini3, graph, n2, r2, calles2);
-  graph->resetGraph();  // se inicializan a 0 las variables de dijkstra
-  auxFunction(ini3, ini1, graph, n3, r3, calles3);
-  graph->resetGraph();  // se inicializan a 0 las variables de dijkstra
+  graph->resetGraph();  // reset dijkstra variables
+  auxFunction(ini2, ini3, graph, n2, r2, streets2);
+  graph->resetGraph();  // reset dijkstra variables
+  auxFunction(ini3, ini1, graph, n3, r3, streets3);
+  graph->resetGraph();  // reset dijkstra variables
 
-  DOUT << "\n\tInfo rutas: n1: " << n1 << ", n2: " << n2 << ", n3: " << n3 << ", nodos iniciales: " << ini1 << ", "
+  DOUT << "\n\tRoute information: n1: " << n1 << ", n2: " << n2 << ", n3: " << n3 << ", initial nodes: " << ini1 << ", "
        << ini2 << ", " << ini3 << endl;
-  DOUT << "\n\t\tRuta 1: " << endl;
+  DOUT << "\n\t\tRoute 1: " << endl;
   for (int i = 0; i < n1; i++) DOUT << r1[i] << " ";
-  DOUT << "\n\t\tRuta 2: " << endl;
+  DOUT << "\n\t\tRoute 2: " << endl;
   for (int i = 0; i < n2; i++) DOUT << r2[i] << " ";
-  DOUT << "\n\t\tRuta 3: " << endl;
+  DOUT << "\n\t\tRoute 3: " << endl;
   for (int i = 0; i < n3; i++) DOUT << r3[i] << " ";
 
-  // se agrupan las 3 rutas en 1
-  ncompleta = n1 + n2 + n3;
-  rsem = new int[ncompleta];
-  streets_ = new int[ncompleta];
-  int pos = 0;
+  // merge 3 routes
+  streetlightNumber = n1 + n2 + n3;
+  streetlightRoute = new int[streetlightNumber];
+  streets = new int[streetlightNumber];
+  int index = 0;
   for (int i = 0; i < n1; i++) {
-    rsem[pos] = r1[i];
-    streets_[pos++] = calles1[i];
+    streetlightRoute[index] = r1[i];
+    streets[index++] = streets1[i];
   }
   for (int i = 0; i < n2; i++) {
-    rsem[pos] = r2[i];
-    streets_[pos++] = calles2[i];
+    streetlightRoute[index] = r2[i];
+    streets[index++] = streets2[i];
   }
   for (int i = 0; i < n3; i++) {
-    rsem[pos] = r3[i];
-    streets_[pos++] = calles3[i];
+    streetlightRoute[index] = r3[i];
+    streets[index++] = streets3[i];
   }
 
-  DOUT << "\n\t\tRuta Completa: " << endl;
-  for (int i = 0; i < ncompleta; i++) DOUT << rsem[i] << " ";
+  DOUT << "\n\t\tRoute completed: " << endl;
+  for (int i = 0; i < streetlightNumber; i++) DOUT << streetlightRoute[i] << " ";
 
-  DOUT << "\n\t\tCalles usadas: " << endl;
-  for (int i = 0; i < ncompleta; i++) {
-    DOUT << streets_[i] << " ";
-    lca.streetList_[streets_[i]]->setColor(1, 0, 1);
+  DOUT << "\n\t\tStreets used: " << endl;
+  for (int i = 0; i < streetlightNumber; i++) {
+    DOUT << streets[i] << " ";
+    streetList.streetList_[streets[i]]->setColor(1, 0, 1);
   }
 
-  cin >> n1;  // borrar esto cuando se compruebe que esta bien
+  cin >> n1;  // TODO borrar esto cuando se compruebe que esta bien
 
   /*
   dijkstra(ini1,graph,true);	//encontrar la route_ optima desde el primer node_ hasta todos los demás
@@ -282,43 +280,43 @@ void Algorithm::greenWave(Graph* graph, int*& rsem, int& ncompleta,
   */
 }
 
-void Algorithm::auxFunction(int inia, int inib, Graph*& graph, int& n, int*& r_, int*& streets_) {
+void Algorithm::auxFunction(int inia, int inib, Graph*& graph, int& n, int*& route, int*& streets_) {
   DOUT << "****************************************************************************" << endl;
-  dijkstra(inia, graph, true);  // encontrar la route_ optima desde el segundo node_ hasta todos los demás
+  dijkstra(inia, graph, true);  // Find best route from node a to each other node
   DOUT << "****************************************************************************" << endl;
 
-  // hacer un pseudo Interaction::setRoute() para conocer la route_ de ini2 a ini3
+  // adapted Interaction::setRoute() to get ini2 to ini3 route
   int aux = inib;
   n = graph->node_[inib].dijkstraVariable_.getIteration();
-  r_ = new int[n];  // vector para almacenar la primera route_
+  route = new int[n];  // array to save first route
   streets_ = new int[n];
 
   for (int i = n - 1; i >= 0; i--) {
-    r_[i] = graph->node_[aux].dijkstraVariable_.getID();
+    route[i] = graph->node_[aux].dijkstraVariable_.getID();
     aux = graph->node_[aux].dijkstraVariable_.getID();
   }
-  // aqui igual hay que aumentar el peso de las aristas que conectan los vertices de la route_ para que no se repitan
+  // TODO aqui igual hay que aumentar el peso de las aristas que conectan los vertices de la route_ para que no se repitan
   // vertices, luego habria que bajar el peso para que se usen en setRuta
   for (int i = 0; i < n; i++) {
-    if (i < (n - 1))  // los primeros indices de la route_ parcial
+    if (i < (n - 1))  // first index of partial route
       for (int j = 0; j < graph->nLinks_; j++) {
-        if (graph->link_[j].getOrigin() == r_[i]) {  // si el node_ anterior o el siguiente está en la route_, se aumenta el
-                                             // peso de la link_ que los une para excluirla de la siguiente route_
+        if (graph->link_[j].getOrigin() == route[i]) {  // if previous or next node is in the route, 
+                                                        //link weight is increased to exclude it from the next route
           graph->link_[j].setVariableWeight(1000);
           streets_[i] = graph->link_[j].getIndex();
         }
-        if (graph->link_[j].getDestination() == r_[i]) graph->link_[j].setVariableWeight(1000);
+        if (graph->link_[j].getDestination() == route[i]) graph->link_[j].setVariableWeight(1000);
       }
-    else  // el ultimo indice de la route_ parcial
+    else  // last node of partial route
       for (int j = 0; j < graph->nLinks_; j++) {
         if (graph->link_[j].getOrigin() ==
-            r_[i]) {  // solo el origen, si el node_ anterior o el siguiente está en la route_, se aumenta el peso de la
-                     // link_ que los une para excluirla de la siguiente route_
+            route[i]) {  // just the origin, if previous or next node is in the route, 
+                         //link weight is increased to exclude it from the next route
           graph->link_[j].setVariableWeight(1000);
           streets_[i] = graph->link_[j].getIndex();
         }
       }
   }
-  DOUT << "\n\t\t***** Salida route_ funcion aux *****";
-  for (int i = 0; i < n; i++) DOUT << "\t" << r_[i] << endl;
+  DOUT << "\n\t\t***** Exit route auxFunction *****";
+  for (int i = 0; i < n; i++) DOUT << "\t" << route[i] << endl;
 }
